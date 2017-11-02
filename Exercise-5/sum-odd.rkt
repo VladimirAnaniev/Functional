ff185e735f51f9be60b55490c839f5ddafc4911e
VladimirAnaniev <vladeto97@gmail.com>
2017-11-02 10:19:17 +0200

    Sum odd

--- Exercise-5/sum-odd.rkt
+++ Exercise-5/sum-odd.rkt
@@ -7,3 +7,9 @@
      acc
      (sum (+ acc (car list)) (cdr list))))
   (sum 0 (filter odd? list)))
+
+(define (sum-odd-short list)
+  (apply + (filter odd? list)))
+
+(define (sum-odd-squares list)
+  (apply + (map (lambda (x) (* x x)) (filter odd? list))))
