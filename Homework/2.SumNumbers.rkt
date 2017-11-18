3dd69cd872653ef79c83ae613ee2f1b6a5831a81
VladimirAnaniev <vladeto97@gmail.com>
2017-11-18 15:35:12 +0200

    Sum-Numbers

---
+++ Homework/2.SumNumbers.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+; Задача 2. Да се напише функция sum-numbers, която приема един аргумент –
+; символен низ и връща сумата на всички числа в него.
+
+
+(define (sum-numbers str)
+  (apply + (filter number? (map string->number (regexp-split #rx"[a-zA-Z]+" str))))
+)
+
+(sum-numbers "a123b2c56"); → 181
+(sum-numbers "a1b2c3"); → 6
