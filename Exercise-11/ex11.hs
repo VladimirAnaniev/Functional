da0bb62b188c3e36e1476a466e7eacf98de26af0
VladimirAnaniev <vladeto97@gmail.com>
2017-12-14 10:45:20 +0200

    Exercise 11

---
+++ Exercise-11/ex11.hs
@@ -0,0 +1,32 @@
+main :: IO()
+main = do
+  print "yo"
+
+maximize :: [(Int -> Int)] -> (Int -> Int)
+maximize xs = \x -> max xs x x
+  where
+  max :: [(Int -> Int)] -> Int -> (Int -> Int)
+  max xs x = foldl (\fun acc -> if abs (fun x) > abs (acc x) then fun else acc) (\x -> 0) xs
+
+digits :: Int -> [(Int)]
+digits n
+  | n < 10 = [n]
+  | otherwise = (digits (n `div` 10)) ++ [(n `mod` 10)]
+
+times :: Int -> Int -> [Int]
+times x 0 = []
+times x n = x:(times x (n - 1))
+
+collatz :: Int -> Int
+collatz 1 = 1
+collatz n
+  | even n = div n 2
+  | otherwise = 3*n + 1
+
+areCollatzSeq :: [Int] -> Bool
+areCollatzSeq (x:[]) = True
+areCollatzSeq xs@(x:y:_) = if collatz x == y then areCollatzSeq (tail xs) else False
+
+--merge :: Ord a => [a] -> [a] -> [a]
+--merge xs ys
+--  | firstx < firstY
